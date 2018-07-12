56e9c010b16c9f05dcb49b198ebed171372879bf
Dan Coventry <daniel.coventry2@gmail.com>
2018-07-12 19:51:47 +0100

    adds a help message

--- src/shortened_url.py
+++ src/shortened_url.py
@@ -5,6 +5,7 @@ def is_shortened_url(shortened_url, xml_root):
     """
     Checks that the requested shortened url exists.
     :param shortened_url: A string to check for if its a valid shortened url.
+    :param xml_root: root of xml tree to be search.
     :return: boolean value for if the shortened url is valid.
     """
     for child in xml_root:
@@ -64,7 +65,7 @@ def add_url_to_xml(short_url, long_url, tree, xml_file):
 
 def alter_short_url(new_short, old_short, tree, xml_file):
     """
-    Alters the short url of a specifed short url.
+    Alters the short url of a specified short url.
     :param old_short: The old url to be replaced.
     :param new_short: The new url to replace the old short url.
     :param tree: The xml tree that the short urlis to be stored in.
