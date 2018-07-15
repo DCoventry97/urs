31284ad34c2579035de589f1d2e0a781b666807b
Dan Coventry <daniel.coventry2@gmail.com>
2018-07-15 22:59:36 +0100

    updated documentation

--- src/make_xml.py
+++ src/make_xml.py
@@ -2,6 +2,10 @@ import xml.etree.ElementTree as ET
 
 
 def make_new_xml_file(xml_file):
+    """
+    Makes an xml file with a root to store the users short words and associated urls.
+    :param xml_file: Name of the xml file to make.
+    """
     root = ET.Element("shortenedUrls")
     tree = ET.ElementTree(root)
     tree.write(xml_file)
